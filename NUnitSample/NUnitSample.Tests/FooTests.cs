c1007ae730063a650bafc42cc74fe1a695fec930
Ragesh Krishna <ragesh@rageshkrishna.com>
2013-04-05 18:20:17 +0530

    Added NUnit sample

---
+++ NUnitSample/NUnitSample.Tests/FooTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitSample.Tests
+{
+    [TestFixture]
+    public class FooTests
+    {
+        [Test]
+        public void CanSayHello()
+        {
+            Foo foo = new Foo();
+            var result = foo.SayHello("World!");
+            Assert.AreEqual("Hello, World!", result);
+        }
+
+    }
+}
