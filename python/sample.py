e5e62e448c137dce5b740f69721da5c66d74d6a1
Ragesh Krishna <rageshk@qhode.com>
2013-08-14 15:47:03 +0530

    Added more python test stuff

--- python/sample.py
+++ python/sample.py
@@ -1,6 +1,17 @@
 import unittest
 
 # Here's our "unit".
+def my_partial_fn(x):       # line 1
+    if x:                   #      2
+        y = 10              #      3
+    return y                #      4
+
+def IsEven(n):
+    if n % 2 == 0:
+        return True
+    else: 
+        return False
+
 def IsOdd(n):
     return n % 2 == 1
 
@@ -12,6 +23,12 @@ class IsOddTests(unittest.TestCase):
 
     def testTwo(self):
         self.failIf(IsOdd(2))
+
+    def testThree(self):
+        self.failIf(IsEven(1))
+
+    def testFour(self):
+        self.failUnless(my_partial_fn(1));
 
 def main():
     unittest.main()
