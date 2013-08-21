f44fa644ce2c3ce3da8c15f7d356fa26eaee37ea
Ragesh Krishna <rageshk@qhode.com>
2013-08-21 14:30:09 +0530

    Updated to use environment variables

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,6 @@
 require 'ci/reporter/rake/rspec' 
 require 'rspec/core/rake_task'
 
-ENV["CI_REPORTS"] = "qhode/testresults"
 RSpec::Core::RakeTask.new(:spec => ["ci:setup:rspec"])
 
 task :default => [:spec]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'simplecov'
 require 'simplecov-csv'
 SimpleCov.formatter = SimpleCov::Formatter::CSVFormatter
+SimpleCov.coverage_dir(ENV["COVERAGE_REPORTS"])
 SimpleCov.start
